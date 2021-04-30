5301b7fdee84067d79001f60b7b08e04cc82874b
Edith Soares <edith.soaares@gmail.com>
2021-04-29 21:41:50 -0300

    Adicionando Getter and Setter

--- Pratica01_Aula05/src/com/company/Pessoa.java
+++ Pratica01_Aula05/src/com/company/Pessoa.java
@@ -12,6 +12,30 @@ public class Pessoa {
         this.cnh = cnh;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
     public void dirigir(){
         System.out.println("Pessoa esta dirigindo...");
     }
